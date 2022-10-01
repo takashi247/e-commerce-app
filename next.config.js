7c46d4c44a8d6c9204028d4cc75311fc98db55e0
takashi247 <takashi247@gmail.com>
2022-10-01 19:06:54 +0900

    add examples until the end of chapter 5

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    // activate styledComponents
-    styledComponents: true,
-  },
+  compiler: (() => {
+    let compilerConfig = {
+      // activate styledComponents
+      styledComponents: true,
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // remove data-testid elements used for React Testing Library in production environment
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+
+    return compilerConfig;
+  }),
 };
 
 module.exports = nextConfig;
